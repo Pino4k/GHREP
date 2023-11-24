75abf4a8b41168e6567d158c8a3d4b0d8056ba83
Presencial <Presencial@DESKTOP-LPHGJF8>
2023-11-24 09:23:16 +0100

    tercer commit (variables)

--- ejemplo_GIT/src/ejemplo_GIT/ejemplo1.java
+++ ejemplo_GIT/src/ejemplo_GIT/ejemplo1.java
@@ -4,5 +4,7 @@ public class ejemplo1 {
 	public static void main(String[]args) {
 		System.out.println("Hola GIT");
 		System.out.println("Chano que flipas con GIT");
+		String Dani="guapu";
+		int num=5984;
 	}
 }
